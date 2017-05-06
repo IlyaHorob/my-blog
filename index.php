ab4d44aa2da4b9ef8ea2fb5830f3aa9c7c7d9182
IlyaHorob <hibestaukro@gmail.com>
2017-05-06 10:22:20 +0300

    Created files structure

--- index.php
+++ index.php
@@ -1,1 +1,28 @@
-<?php
+<?php include_once 'header.php' ?>
+<?php include_once 'navbar.php' ?>
+<?php include_once 'menu.php' ?>
+<div class="content-wrapper">
+    <div class="container">
+        <div class="row">
+            <div class="col-md-12">
+                <h4 class="page-head-line">Blank Page</h4>
+            
+            </div>
+        
+        </div>
+        <div class="row">
+            <div class="col-md-12">
+                <div class="alert alert-warning">
+                    This is blank page for which you can customize for your project.
+                    Use this admin template 100% free to use for personal and commercial use which is crafted by
+                    <br />
+                    <a href="http://www.designbootstrap.com/" target="_blank">DesignBootstrap.com</a> . For more free templates and snippets keep looking <a href="http://www.designbootstrap.com/" target="_blank">DesignBootstrap.com</a> . Hope you will like our work
+                
+                </div>
+            </div>
+        
+        </div>
+    </div>
+</div>
+<!-- CONTENT-WRAPPER SECTION END-->
+<?php include_once 'footer.php' ?>
